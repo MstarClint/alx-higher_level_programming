5924870a7027885ec850ff7e6ac6d04bc81fdb0f
MstarClint <obiohaclinton111@gmail.com>
2024-01-05 05:48:37 +0100

    function that checks if a singly linked list has a cycle in it

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,116 +1,25 @@
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
-#include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
- */
-size_t print_listint(const listint_t *h)
-{
-	const listint_t *current;
-	unsigned int n; /* number of nodes */
-
-	current = h;
-	n = 0;
-	while (current != NULL)
-	{
-	printf("%i\n", current->n);
-	current = current->next;
-	n++;
-	}
-
-	return (n);
-}
-
-/**
- * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: pointer to a pointer of the start of the list
- * @n: integer to be included in node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-	return (NULL);
-
-	new->n = n;
-	new->next = *head;
-	*head = new;
-
-	return (new);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-	listint_t *current;
-
-	while (head != NULL)
-	{
-	current = head;
-	head = head->next;
-	free(current);
-	}
-carrie@ubuntu:~/0x00$ cat 10-main.c
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - check the code
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Return: Always 0.
+ * Description: singly linked list node structure
+ * 
  */
-int main(void)
+typedef struct listint_s
 {
-	listint_t *head;
-	listint_t *current;
-	listint_t *temp;
-	int i;
+    int n;
+    struct listint_s *next;
+} listint_t;
 
-	head = NULL;
-	add_nodeint(&head, 0);
-	add_nodeint(&head, 1);
-	add_nodeint(&head, 2);
-	add_nodeint(&head, 3);
-	add_nodeint(&head, 4);
-	add_nodeint(&head, 98);
-	add_nodeint(&head, 402);
-	add_nodeint(&head, 1024);
-	print_listint(head);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
-	if (check_cycle(head) == 0)
-	printf("Linked list has no cycle\n");
-	else if (check_cycle(head) == 1)
-	printf("Linked list has a cycle\n");
-
-	current = head;
-	for (i = 0; i < 4; i++)
-	current = current->next;
-	temp = current->next;
-	current->next = head;
-
-	if (check_cycle(head) == 0)
-	printf("Linked list has no cycle\n");
-	else if (check_cycle(head) == 1)
-	printf("Linked list has a cycle\n");
-
-	current = head;
-	for (i = 0; i < 4; i++)
-	current = current->next;
-	current->next = temp;
-
-	free_listint(head);
-
-	return (0);
-}
+#endif /* LISTS_H */
